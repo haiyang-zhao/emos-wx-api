2e6c9da583855284da04f1e4081c255d8ce20d1d
mactty <mactty@com>
2022-05-02 10:25:15 +0800

    读取系统常量.

--- src/main/java/com/zhy/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/zhy/emos/wx/EmosWxApiApplication.java
@@ -1,15 +1,45 @@
 package com.zhy.emos.wx;
 
+import cn.hutool.core.util.StrUtil;
+import com.zhy.emos.wx.config.SystemConstants;
+import com.zhy.emos.wx.db.dao.SysConfigDao;
+import com.zhy.emos.wx.db.pojo.SysConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.util.List;
+
 @SpringBootApplication
 @ServletComponentScan
+@Slf4j
 public class EmosWxApiApplication {
+    @Autowired
+    private SysConfigDao sysConfigDao;
+
+    @Autowired
+    private SystemConstants systemConstants;
 
     public static void main(String[] args) {
         SpringApplication.run(EmosWxApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        List<SysConfig> sysConfigs = sysConfigDao.selectAll();
+        sysConfigs.forEach(item -> {
+            String key = StrUtil.toCamelCase(item.getParamKey());
+            String value = item.getParamValue();
+            try {
+                Field field = systemConstants.getClass().getDeclaredField(key);
+                field.set(systemConstants, value);
+            } catch (Exception e) {
+                log.error("init error", e);
+            }
+        });
+    }
 }
--- src/main/java/com/zhy/emos/wx/db/dao/SysConfigDao.java
+++ src/main/java/com/zhy/emos/wx/db/dao/SysConfigDao.java
@@ -1,8 +1,12 @@
 package com.zhy.emos.wx.db.dao;
 
+import com.zhy.emos.wx.db.pojo.SysConfig;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface SysConfigDao {
+    List<SysConfig> selectAll();
 
 }
